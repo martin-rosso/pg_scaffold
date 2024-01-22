8aa992daa2b0d84bcfe8d07a1dd6d4fe53a889f4
Martín Rosso <mrosso10@gmail.com>
2024-01-22 17:05:55 -0300

    rails new plugin

---
+++ lib/pg_scaffold/railtie.rb
@@ -0,0 +1,4 @@
+module PgScaffold
+  class Railtie < ::Rails::Railtie
+  end
+end
---
+++ test/pg_scaffold_test.rb
@@ -0,0 +1,7 @@
+require "test_helper"
+
+class PgScaffoldTest < ActiveSupport::TestCase
+  test "it has a version number" do
+    assert PgScaffold::VERSION
+  end
+end
